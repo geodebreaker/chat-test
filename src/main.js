914cbc3a2b97aa2dd25682bb919bee04cc059939
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-08-15 23:58:03 +0000

    modified:   src/index.html 	modified:   src/main.js 	modified:   src/style.css

--- src/main.js
+++ src/main.js
@@ -1,16 +1,50 @@
-$=x=>document.querySelector(x);
+$ = x => document.querySelector(x);
 
-$('#msg').onkeypress=x=>{if(x.key=='Enter')$('#send').click()};
-$('#send').onclick=x=>send($('#msg').value);
+$('#msg').onkeypress = x => { if (x.key == 'Enter') $('#send').click() };
+$('#send').onclick = x => send($('#msg').value);
+$('#leave').onclick = x => $('#login').showPopover();
+
+$('#login').showPopover();
 
 var ws;
 
-function send(value){
+function send(value) {
   console.log(value);
-  ws.send(JSON.stringify())
+  ws.send(JSON.stringify({send: k}));
 }
 
-function recv(value){
-  console.log(value)
+function recv(value) {
+  console.log(value);
+  var x = JSON.parse(value);
+  x = x[Object.keys(x)[0]];
+  mkmsg(x.from, x.data);
 }
 
+function mkmsg(from, data) {
+  var u = document.createElement('span');
+  u.innerText = from + ': ';
+  u.className = 'usertag';
+  u.style.color = colorhash(from);
+  var m = document.createElement('span');
+  m.innerText = data;
+  $('#chat').innerHTML += u.outerHTML + m.outerHTML + '<br>';
+}
+
+function colorhash(x) {
+  var w = x.split('');
+  var y = 0;
+  for (var z of w) {
+    y += z.charCodeAt(0);
+  }
+  y = y / 1000 + y;
+  y *= 202383492;
+  y %= 10284672;
+  y *= 846589213;
+  y %= 38357692;
+  y *= 84748;
+  return '#' + Math.floor(y % 0xffffff).toString(16);
+}
+
+function setupws(){
+  ws = new WebSocket();
+}
