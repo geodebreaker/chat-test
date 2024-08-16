77c267814b48a15d41995621168cbf5ce2810901
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-08-16 15:59:50 +0000

    modified:   server/node.js 	modified:   src/index.html 	modified:   src/main.js 	modified:   src/style.css

--- src/main.js
+++ src/main.js
@@ -5,21 +5,30 @@ var un;
 var room;
 var loggedin = false;
 
+if (window.localStorage) {
+  un = localStorage.un;
+  room = localStorage.room;
+}
+
+$('#username').onkeypress = x => {
+  if (x.key == 'Enter') $('#room').focus();
+  $('#username').style.color = colorhash($('#username').value + (x.key.length == 1 ? x.key : ''))
+};
 $('#msg').onkeypress = x => { if (x.key == 'Enter') $('#send').click() };
+$('#room').onkeypress = x => { if (x.key == 'Enter') $('#libtn').click() };
 $('#send').onclick = x => send($('#msg').value);
-$('#leave').onclick = x => { $('#login').showPopover(); loggedin = false; ws.close() };
+$('#leave').onclick = x => { $('#login').showPopover(); loggedin = null; ws.close() };
 $('#libtn').onclick = x => login();
+$('#un');
 
 $('#login').showPopover();
 
 function send(value) {
-  console.log(value);
   ws.send(JSON.stringify({ msg: value }));
   mkmsg(un, value);
 }
 
 function recv(value) {
-  console.log(value);
   mkmsg(value.from, value.data);
 }
 
@@ -33,24 +42,57 @@ function mkmsg(from, data) {
   $('#chat').innerHTML += u.outerHTML + m.outerHTML + '<br>';
 }
 
+function mkalert(type, data, un) {
+  var m = document.createElement('span');
+  m.innerText = data;
+  m.className = type ? 'good' : 'bad';
+  var u = document.createElement('span');
+  u.innerText = un;
+  u.className = 'usertag';
+  u.style.color = colorhash(un);
+  $('#chat').innerHTML += m.outerHTML + u.outerHTML + '<br>';
+}
+
 function colorhash(x) {
+  var r = x => parseInt(('' + x).split('').reverse().join(''));
   var w = x.split('');
   var y = 0;
   for (var z of w) {
     y += z.charCodeAt(0);
   }
-  y = y / 1000 + y;
+  y = Math.floor(y / 10000 + y);
+  y = r(y);
   y *= 202383492;
-  y %= 10284672;
-  y *= 846589213;
-  y %= 38357692;
+  y %= 10284631;
+  y *= 846689351;
+  y = r(y);
+  y %= 38357492;
   y *= 84748;
-  return '#' + Math.floor(y % 0xffffff).toString(16);
+  y = r(y);
+  var v = [];
+  var u = 0;
+  for (var i = 0; i < 3; i++) {
+    y = Math.floor(y / 255);
+    v.push(y % 255);
+    u += y % 255;
+  }
+  u /= 3;
+  if (u <= 100) {
+    v = v.map(x => 255 - x);
+  }
+  return '#' + v
+    .map(x => x.toString(16))
+    .map(x => "0".repeat(Math.max(0, 2 - x.length)) + x)
+    .join('');
 }
 
 function login() {
   un = $('#username').value;
   room = $('#room').value;
+  if (window.localStorage) {
+    localStorage.un = un;
+    localStorage.room = room;
+  }
   ws = new WebSocket((window.location.protocol == 'http:' ? 'ws://' : 'wss://') + window.location.host);
   ws.onmessage = value => {
     var x = JSON.parse(value.data);
@@ -61,6 +103,7 @@ function login() {
         if (x == "") {
           $('#login').hidePopover();
           $('#undisplay').innerText = un;
+          $('#undisplay').style.color = colorhash(un);
           $('#roomdisplay').innerText = room;
           loggedin = true;
         } else {
@@ -69,6 +112,12 @@ function login() {
         break;
       case 'msg':
         recv(x);
+        break;
+      case 'connect':
+        mkalert(false, 'connected: ', x);
+        break;
+      case 'disconnect':
+        mkalert(true, 'disconnected: ', x);
         break;
     }
   };
@@ -84,7 +133,7 @@ function login() {
     if (loggedin) {
       $('#login').showPopover();
       $('#login div').innerText = 'disconnected. please reload';
-    } else {
+    } else if (loggedin !== null) {
       $('#lilog').innerText = 'failed to sign in: failed to connect to server';
     }
   }
