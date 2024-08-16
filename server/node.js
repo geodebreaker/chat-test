d854a9ff978abd410cd7cfd7000927614e0a3718
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-08-16 14:25:32 +0000

    modified:   server/node.js 	new file:   server/package-lock.json 	modified:   server/package.json 	modified:   src/index.html 	modified:   src/main.js 	modified:   src/style.css

--- server/node.js
+++ server/node.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const ws = require('ws');
 
-http.createServer((req, res) => {
+const svr = http.createServer((req, res) => {
   var url = req.url;
   url = url.replace(/\.\./g, '.');
   url = url.replace(/\/$/, '/index.html');
@@ -15,13 +16,75 @@ http.createServer((req, res) => {
       res.writeHead(200, {
         'Content-Type':
           (
-            f('js') ? 'application/javascript' : 
-            f('css') ? 'text/css' : 
-            f('gif') ? 'image/gif' : 
-            'text/html'
+            f('js') ? 'application/javascript' :
+              f('css') ? 'text/css' :
+                f('gif') ? 'image/gif' :
+                  'text/html'
           )
       });
       res.end(data);
     }
   });
-}).listen(8080);
+});
+
+var wss = ws.createServer({ server: svr });
+var clients = {};
+
+function send(ws, type, data) {
+  console.log(`Sent to ${ws.un}: ${type} :`, data);
+
+  var x = {};
+  x[type] = value;
+  ws.send(JSON.stringify(x));
+}
+
+function emit(type, data, room, exclude) {
+  for (var un in clients) {
+    if (clients[un].room == room && un != exclude)
+      send(clients[un], type, data);
+  }
+}
+
+wss.on('connection', (ws) => {
+  ws.li = false;
+  ws.un = '';
+  ws.room = null;
+
+  ws.on('message', (message) => {
+    console.log(`Received from ${ws.un}: ${message}`);
+
+    var x = JSON.parse(message.data);
+    var y = Object.keys(x)[0];
+    x = x[y];
+
+    switch(y){
+      case 'li':
+        if(clients[y.username] || clients[y.username].length < 2 || clients[y.username].length > 12){
+          return send(ws, li, 'bad username');
+        }
+        
+        ws.un = y.username;
+        ws.room = y.room;
+        ws.li = true;
+        emit('connect', ws.un, ws.room, ws.un);
+
+        break;
+      case 'msg':
+
+        emit('msg', {from:ws.un,data:y}, ws.room, ws.un);
+
+        break;
+    }
+  });
+
+  // Event handler for WebSocket connection closing
+  ws.on('close', () => {
+    if (ws.li) {
+      emit('disconnected', ws.un, ws.room, ws.un)
+    }
+    delete clients[ws.un];
+  });
+});
+
+
+svr.listen(8080);
