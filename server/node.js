427032affb328a7e2507517235288f1dc017f68b
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-08-16 14:40:48 +0000

    modified:   server/node.js 	modified:   src/main.js

--- server/node.js
+++ server/node.js
@@ -27,19 +27,20 @@ const svr = http.createServer((req, res) => {
   });
 });
 
-var wss = ws.createServer({ server: svr });
+var wss = new ws.Server({ server: svr });
 var clients = {};
 
 function send(ws, type, data) {
   console.log(`Sent to ${ws.un}: ${type} :`, data);
 
   var x = {};
-  x[type] = value;
+  x[type] = data;
   ws.send(JSON.stringify(x));
 }
 
 function emit(type, data, room, exclude) {
   for (var un in clients) {
+    console.log(un)
     if (clients[un].room == room && un != exclude)
       send(clients[un], type, data);
   }
@@ -53,25 +54,28 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log(`Received from ${ws.un}: ${message}`);
 
-    var x = JSON.parse(message.data);
+    var x = JSON.parse(message);
     var y = Object.keys(x)[0];
     x = x[y];
 
     switch(y){
       case 'li':
-        if(clients[y.username] || clients[y.username].length < 2 || clients[y.username].length > 12){
-          return send(ws, li, 'bad username');
+        if(clients[x.username] || x.username.length < 2 || x.username.length > 12){
+          return send(ws, 'li', 'bad username');
         }
         
-        ws.un = y.username;
-        ws.room = y.room;
+        ws.un = x.username;
+        ws.room = x.room;
         ws.li = true;
         emit('connect', ws.un, ws.room, ws.un);
+        send(ws, 'li', '');
+        console.log(`${ws.un} logged into room ${ws.room}`);
+        clients[ws.un] = ws;
 
         break;
       case 'msg':
 
-        emit('msg', {from:ws.un,data:y}, ws.room, ws.un);
+        emit('msg', {from:ws.un,data:x}, ws.room, ws.un);
 
         break;
     }
@@ -79,6 +83,7 @@ wss.on('connection', (ws) => {
 
   // Event handler for WebSocket connection closing
   ws.on('close', () => {
+    console.log('Disconnected:', ws.un)
     if (ws.li) {
       emit('disconnected', ws.un, ws.room, ws.un)
     }
