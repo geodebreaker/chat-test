6689811956ecf4c5be6e250bded3094a1ad2a316
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-08-31 21:15:33 +0000

    modified:   server/node.js 	modified:   server/package-lock.json 	modified:   server/package.json 	modified:   src/index.html 	modified:   src/main.js 	modified:   src/style.css

--- server/node.js
+++ server/node.js
@@ -1,17 +1,86 @@
+require('dotenv').config();
 const http = require('http');
 const fs = require('fs');
 const ws = require('ws');
+const mysql = require('mysql2');
+
+process.on('unhandledExeption', (reason) => {
+  console.error(reason);
+})
+
+process.on('unhandledRejection', (reason) => {
+  console.error(reason);
+})
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'app',
+  port: 3306
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.error('Error connecting to DB:\n ', err.stack);
+    return;
+  }
+  console.log('Connected to DB');
+});
+
+setInterval(() => {
+  conn.query('SELECT 1', (error) => {
+    if (error) {
+      console.error('Keep-alive query error:', error);
+    } else {
+      console.log('Keep-alive query executed');
+    }
+  });
+}, 300000);  // 5 minutes
+
+function getRoomData(room) {
+  return new Promise((y, n) => {
+    const sql = 'SELECT * FROM msg WHERE room=?';
+      conn.query(sql, [room], (error, results) => {
+        console.log(`Got room data for room ${room}`)
+        y(results
+          .map(x => ({ date: Date.parse(x.date), user: getUser(x.user), text: x.text, id: x.id }))
+          .sort((a, b) => a.date - b.date)
+        );
+      });
+  });
+}
+
+function putMsg(user, room, text) {
+  return new Promise((y, n) => {
+    const sql = 'INSERT INTO msg (user, room, text) VALUES (?, ?, ?)';
+      conn.query(sql, [user, room, text], (error, results) => {
+        if (error) {
+          n(error);
+          return;
+        }
+        console.log(`Put message ${text} from ${user} in ${room}`)
+        y(results.insertId);
+      });
+  });
+}
+
+//TODO: add user system
+function getUser(id) {
+  console.log(`Got user ${id} for id ${id}`);
+  return id;
+}
 
 const svr = http.createServer((req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Request-Method', '*');
-	res.setHeader('Access-Control-Allow-Methods', '*');
-	res.setHeader('Access-Control-Allow-Headers', '*');
-	if ( req.method === 'OPTIONS' ) {
-		res.writeHead(200);
-		res.end();
-		return;
-	}
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
   var url = req.url;
   url = url.replace(/\.\./g, '.');
   url = url.replace(/\/$/, '/index.html');
@@ -31,22 +100,22 @@ const svr = http.createServer((req, res) => {
     });
   };
 
-  fetch('https://cdn.jsdelivr.net/gh/geodebreaker/chat-test/src'+url)
-    .then(x => {
-      s();
-      return x.text();
-    }).then(x => res.end(x));
+  // fetch('https://geodebreaker.github.io/chat-test/src/' + url)
+  //   .then(x => {
+  //     res.writeHead(200, { 'Content-Type': x.headers.get('Content-Type') });
+  //     return x.text();
+  //   }).then(x => res.end(x));
 
-  // fs.readFile('../src/' + url, (err, data) => {
-  //   console.log(err ? 'fail:' : 'success:', url);
-  //   if (err) {
-  //     res.writeHead(404, { 'Content-Type': 'text/html' });
-  //     res.end('404: File not found');
-  //   } else {
-  //     s();
-  //     res.end(data);
-  //   }
-  // });
+  fs.readFile('../src/' + url, (err, data) => {
+    console.log(err ? 'fail:' : 'success:', url);
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('404: File not found');
+    } else {
+      s();
+      res.end(data);
+    }
+  });
 });
 
 var wss = new ws.Server({ server: svr });
@@ -92,11 +161,17 @@ wss.on('connection', (ws) => {
         send(ws, 'li', '');
         console.log(`${ws.un} logged into room ${ws.room}`);
         clients[ws.un] = ws;
+        getRoomData(ws.room).then(x =>
+          send(ws, 'roommsg', x)
+        );
 
         break;
       case 'msg':
 
-        emit('msg', { from: ws.un, data: x }, ws.room, ws.un);
+        putMsg(ws.un, ws.room, x.value).then(id => {
+          emit('msg', { from: ws.un, data: x.value, id: x.value, date: Date.now() }, ws.room, ws.un);
+          send(ws, 'updateid', { tmpid: x.tmpid, newid: id });
+        });
 
         break;
       case 'users':
