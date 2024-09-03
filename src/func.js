ccd4c5915e14a01406424e1428582ab9b4513850
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-09-03 16:07:56 +0000

    modified:   server/node.js 	new file:   src/func.js 	modified:   src/index.html 	modified:   src/main.js 	new file:   src/net.js 	modified:   src/style.css

---
+++ src/func.js
@@ -0,0 +1,97 @@
+function ontab() {
+  return document.visibilityState === 'visible' && document.hasFocus();
+};
+
+function fmtDate(ms) {
+  var x = new Date(parseInt(ms));
+  var y = x.getHours() % 12;
+  var z = x.getMinutes().toString();
+  return `${x.getMonth()}/${x.getDay()}/${x.getFullYear()} ` +
+    `${y == 0 ? 12 : y}:${z.length == 1 ? '0' + z : z} ${x.getHours() > 11 ? 'PM' : 'AM'}`;
+}
+
+function colorhash(x) {
+  var r = x => parseInt(('' + x).split('').reverse().join(''));
+  var w = x.split('');
+  var y = 0;
+  for (var z of w) {
+    y += z.charCodeAt(0);
+  }
+  y = Math.floor(y / 10000 + y);
+  y = r(y);
+  y *= 202383492;
+  y %= 10284631;
+  y *= 846689351;
+  y = r(y);
+  y %= 38357492;
+  y *= 84748;
+  y = r(y);
+  var v = [];
+  var u = 0;
+  for (var i = 0; i < 3; i++) {
+    y = Math.floor(y / 255);
+    v.push(y % 255);
+    u += y % 255;
+  }
+  u /= 3;
+  if (u <= 100) {
+    v = v.map(x => 255 - x);
+  }
+  return '#' + v
+    .map(x => x.toString(16))
+    .map(x => "0".repeat(Math.max(0, 2 - x.length)) + x)
+    .join('');
+}
+
+function rclick(event) {
+  if (event.target.tagName == 'A' || event.target.tagName == 'IMG')
+    return;
+  event.preventDefault();
+  $('#rclick').style.left = event.clientX + 'px';
+  $('#rclick').style.top = event.clientY + 'px';
+  $('#rclick').dataset.id = this.dataset.id;
+  $('#rc-date').innerText = fmtDate(this.dataset.date);
+  $('#rc-copy').onclick = () => { copyText(this.dataset.text) };
+  $('#rclick').showPopover();
+}
+
+function clickLink(ev, t) {
+  if (!confirm(`Do you want to go to "${t}"`))
+    ev.preventDefault();
+}
+
+function styleMsg(x) {
+  var y = x
+    .replace(/!/g, '!!')
+    .replace(/[<&"]/g, x => '!' + x)
+    .replace(
+      /\^(.*?)(?<!\\);/g, (x, y) => {
+        var z = y.split(/(?<!\\),/g);
+        z = styles(z.shift(), z);
+        return z ?? x;
+      }
+    )
+    .replace(/(!*?)!([<&"])/g, (x, z, y) =>
+      z + (z.length % 2 == 1 ? y : { '<': '&lt;', '&': '&amp;', '"': '&quot;' }[y])
+    ).replace(/!!/g, '!');
+  return y;
+}
+
+function styles(x, y) {
+  switch (x) {
+    case 'l':
+    case 'ls':
+      return ('<a href="?" onclick="clickLink(event, ?)" target="?">?</a>'
+        .replace('?', y[0].replace(/^(#)?(?:http(s)?:\/\/)?/, (x, y, z) => y ? x : 'http' + (z ?? '') + '://'))
+        .replace('?', y[0].startsWith('#')?"'"+y[0]+"'":this.href)
+        .replace('?', x == 'l' ? '_blank' : '')
+        .replace('?', y[1] ?? y[0])
+      );
+    case '':
+      break;
+  }
+}
+
+function copyText(x) {
+
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,49 @@
 $ = x => document.querySelector(x);
 $$ = x => document.querySelectorAll(x);
 
+function init(){
+  if (window.localStorage && localStorage.un) {
+    un = localStorage.un;
+    $('#username').value = un;
+    $('#password').value = localStorage.pw;
+    room = localStorage.room;
+    $('#room').value = room;
+  }
+
+  document.addEventListener('click', e => {
+    if (!(e.target == $('#rclick') || $('#rclick').contains(e.target)))
+      $('#rclick').hidePopover();
+  });
+  window.onfocus = () => { notif = 0; updateTitle() };
+  $('#username').onkeypress = x => {
+    if (x.key == 'Enter') $('#password').focus();
+    $('#username').style.color = colorhash($('#username').value + (x.key.length == 1 ? x.key : ''))
+  };
+  $('#password').onkeypress = x => { if (x.key == 'Enter') $('#room').focus() };
+  $('#msg').onkeypress = x => { if (x.key == 'Enter') $('#send').click() };
+  $('#room').onkeypress = x => { if (x.key == 'Enter') $('#libtn').click() };
+  $('#send').onclick = x => send($('#msg').value);
+  $('#leave').onclick = x => {
+    $('#login').showPopover();
+    loggedin = null;
+    ws.close();
+    $('#chat').innerHTML = '';
+    userlist = [];
+    updateTitle();
+  };
+  $('#libtn').onclick = x => login();
+  $('#openmenu').onclick = x => {
+    menuopen = !menuopen;
+    $('body').style.gridTemplateAreas =
+      "'u r r l o' 'c c " + (menuopen ? "x x x" : "c c c") + "' 'm m m s s'";
+    $('#menu').style.display = (menuopen ? 'block' : 'none');
+    if (menuopen)
+      ws.send(JSON.stringify({ users: '' }));
+  };
+
+  $('#login').showPopover();
+}
+
 var ws;
 var un;
 var room;
@@ -8,51 +51,6 @@ var notif = 0;
 var loggedin = false;
 var menuopen = false;
 var userlist = [];
-
-if (window.localStorage && localStorage.un) {
-  un = localStorage.un;
-  $('#username').value = un;
-  $('#password').value = localStorage.pw;
-  room = localStorage.room;
-  $('#room').value = room;
-}
-
-document.addEventListener('click', e => {
-  if (!(e.target == $('#rclick') || $('#rclick').contains(e.target)))
-    $('#rclick').hidePopover();
-});
-window.onfocus = () => { notif = 0; updateTitle() };
-$('#username').onkeypress = x => {
-  if (x.key == 'Enter') $('#password').focus();
-  $('#username').style.color = colorhash($('#username').value + (x.key.length == 1 ? x.key : ''))
-};
-$('#password').onkeypress = x => { if (x.key == 'Enter') $('#room').focus() };
-$('#msg').onkeypress = x => { if (x.key == 'Enter') $('#send').click() };
-$('#room').onkeypress = x => { if (x.key == 'Enter') $('#libtn').click() };
-$('#send').onclick = x => send($('#msg').value);
-$('#leave').onclick = x => {
-  $('#login').showPopover();
-  loggedin = null;
-  ws.close();
-  $('#chat').innerHTML = '';
-  userlist = [];
-  updateTitle();
-};
-$('#libtn').onclick = x => login();
-$('#openmenu').onclick = x => {
-  menuopen = !menuopen;
-  $('body').style.gridTemplateAreas =
-    "'u r r l o' 'c c " + (menuopen ? "x x x" : "c c c") + "' 'm m m s s'";
-  $('#menu').style.display = (menuopen ? 'block' : 'none');
-  if (menuopen)
-    ws.send(JSON.stringify({ users: '' }));
-};
-
-$('#login').showPopover();
-
-function ontab() {
-  return document.visibilityState === 'visible' && document.hasFocus();
-};
 
 function send(value) {
   if (value != '') {
@@ -77,18 +75,18 @@ function mkmsg(from, data, id, date, tag) {
   u.innerHTML += `<span class="tag _${tag}"></span>` + ': ';
   u.className = 'usertag';
   u.style.color = colorhash(from);
-  u.setAttribute('oncontextmenu', 'rclick(event)');
-  u.dataset.id = id;
-  u.dataset.date = date;
 
   var m = document.createElement('span');
-  m.innerText = data;
-  m.oncontextmenu = 'rclick';
-  m.setAttribute('oncontextmenu', 'rclick(event)');
-  m.dataset.id = id;
-  m.dataset.date = date;
+  m.innerHTML = styleMsg(data);
 
-  $('#chat').innerHTML += u.outerHTML + m.outerHTML + '<br>';
+  var c = document.createElement('span');
+  c.oncontextmenu = rclick;
+  c.dataset.id = id;
+  c.dataset.date = date;
+  c.dataset.text = data;
+  c.innerHTML = u.outerHTML + m.outerHTML + '<br>';
+
+  $('#chat').appendChild(c);
   updateChat();
 
   if (!ontab()) {
@@ -101,11 +99,16 @@ function mkalert(type, data, un) {
   var m = document.createElement('span');
   m.innerText = data;
   m.className = type ? 'bad' : 'good';
+
   var u = document.createElement('span');
   u.innerText = un;
   u.className = 'usertag';
   u.style.color = colorhash(un);
-  $('#chat').innerHTML += m.outerHTML + u.outerHTML + '<br>';
+
+  var c = document.createElement('span');
+  c.innerHTML = m.outerHTML + u.outerHTML + '<br>';
+
+  $('#chat').appendChild(c);
   updateChat();
 
   if (!ontab()) {
@@ -137,136 +140,4 @@ function updateTitle() {
     (loggedin ? ' - ' + room : '');
 }
 
-function rclick(event) {
-  event.preventDefault();
-  $('#rclick').style.left = event.clientX + 'px';
-  $('#rclick').style.top = event.clientY + 'px';
-  $('#rc-date').innerText = fmtDate(event.target.dataset.date);
-  $('#rclick').showPopover();
-}
-
-function fmtDate(ms) {
-  var x = new Date(parseInt(ms));
-  var y = x.getHours() % 12;
-  var z = x.getMinutes().toString();
-  return `${x.getMonth()}/${x.getDay()}/${x.getFullYear()} ` +
-    `${y == 0 ? 12 : y}:${z.length == 1 ? '0' + z : z} ${x.getHours() > 11 ? 'PM' : 'AM'}`;
-}
-
-function colorhash(x) {
-  var r = x => parseInt(('' + x).split('').reverse().join(''));
-  var w = x.split('');
-  var y = 0;
-  for (var z of w) {
-    y += z.charCodeAt(0);
-  }
-  y = Math.floor(y / 10000 + y);
-  y = r(y);
-  y *= 202383492;
-  y %= 10284631;
-  y *= 846689351;
-  y = r(y);
-  y %= 38357492;
-  y *= 84748;
-  y = r(y);
-  var v = [];
-  var u = 0;
-  for (var i = 0; i < 3; i++) {
-    y = Math.floor(y / 255);
-    v.push(y % 255);
-    u += y % 255;
-  }
-  u /= 3;
-  if (u <= 100) {
-    v = v.map(x => 255 - x);
-  }
-  return '#' + v
-    .map(x => x.toString(16))
-    .map(x => "0".repeat(Math.max(0, 2 - x.length)) + x)
-    .join('');
-}
-
-function login() {
-  un = $('#username').value;
-  room = $('#room').value;
-  var pw = $('#password').value;
-  if (window.localStorage) {
-    localStorage.un = un;
-    localStorage.room = room;
-    localStorage.pw = pw;
-  }
-  ws = new WebSocket((window.location.protocol == 'http:' ? 'ws://' : 'wss://') + window.location.host);
-  ws.onmessage = value => {
-    var x = JSON.parse(value.data);
-    var y = Object.keys(x)[0];
-    x = x[y];
-    console.log(y + ':', x);
-    switch (y) {
-      case 'li':
-        if (x == "") {
-          $('#login').hidePopover();
-          $('#undisplay').innerText = un;
-          $('#undisplay').style.color = colorhash(un);
-          $('#roomdisplay').innerText = room;
-          loggedin = true;
-          updateTitle()
-        } else {
-          $('#lilog').innerText = 'failed to sign in: ' + x;
-        }
-        break;
-      case 'msg':
-        recv(x);
-        break;
-      case 'connect':
-        mkalert(false, 'connected: ', x[0]);
-        userlist.push(x);
-        updateMenu();
-        break;
-      case 'disconnect':
-        mkalert(true, 'disconnected: ', x[0]);
-        userlist.splice(userlist.findIndex(z => z[0] == x[0]), 1);
-        updateMenu();
-        break;
-      case 'users':
-        userlist = x;
-        updateMenu();
-        break;
-      case 'roommsg':
-        x.map(m => mkmsg(m.user, m.text, m.id, m.date, m.tag));
-        break;
-      case 'updateid':
-        $$(`[data-id="${x.tmpid}"]`).forEach(y => y.dataset.id = x.newid)
-        break;
-      case 'remmsg':
-        $$(`[data-id="${x}"]`).forEach(y => y.remove());
-        break;
-    }
-  };
-  ws.onopen = () => {
-    ws.send(JSON.stringify({
-      li: {
-        username: un,
-        password: pw,
-        room,
-      }
-    }))
-  };
-  ws.onclose = (x) => {
-    if (loggedin) {
-      $('#login').showPopover();
-      $('#login div').innerText = 'disconnected. please reload';
-      loggedin = false;
-      updateTitle();
-    } else if (loggedin !== null) {
-      $('#lilog').innerText = 'failed to sign in: failed to connect to server';
-    }
-  }
-}
-
-setInterval(() => {
-  if (ws && ws.readyState == ws.OPEN)
-    ws.send(JSON.stringify({ ping: '' }))
-}, 60e3);
-
-var ping;
-fetch('ping.mp3').then(x => x.blob()).then(x => ping = URL.createObjectURL(x));
+init();
