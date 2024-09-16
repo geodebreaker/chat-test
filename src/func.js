67b68c12b480149397e3debda271ebd679c48e9b
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-09-16 13:19:28 -0500

    Update func.js

--- src/func.js
+++ src/func.js
@@ -302,7 +302,7 @@ function handleCmd(cmd, args) {
 }
 
 function styleEmote(x) {
-  const emo = [ "mood", "goober", "horror", "nohorror", "clueless", "silly", "roll" ];
+  const emo = [ "mood", "goober", "horror", "nohorror", "clueless", "silly", "roll", "mh" ];
 
   return x.replace(/(\\?)(:(.{4,8}?):)/g, (_match, bs, og, name) =>
     bs ? og : emo.includes(name) ? '^p,' + `https://evrtdg.com/emoji/${name}.jpg` + ',emote;' : og);
